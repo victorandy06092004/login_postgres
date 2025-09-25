a3ceeae8f36ca3f0943b84afb1707a331c9eb40c
unknown <VICTOR@BP-ATEQUE.badinotti.pe>
2025-09-25 10:04:33 -0500

    cambios hasta editar_usuario y funcionamiento

--- editar_usuario.php
+++ editar_usuario.php
@@ -7,16 +7,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gmail = $_POST['gmail'];
     $contrasena = $_POST['contrasena'];
     $estado = ($_POST['estado'] === "true") ? true : false;
+    $id_rol = intval($_POST['id_rol']); // nuevo: rol
 
     try {
         $sql = "UPDATE usuarios 
-                SET nombre = :nombre, gmail = :gmail, contrasena = :contrasena, estado = :estado 
+                SET nombre = :nombre, gmail = :gmail, contrasena = :contrasena, estado = :estado, id_rol = :id_rol 
                 WHERE id = :id";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':nombre', $nombre);
         $stmt->bindParam(':gmail', $gmail);
         $stmt->bindParam(':contrasena', $contrasena);
         $stmt->bindParam(':estado', $estado, PDO::PARAM_BOOL);
+        $stmt->bindParam(':id_rol', $id_rol, PDO::PARAM_INT); // enlazar rol
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
 
